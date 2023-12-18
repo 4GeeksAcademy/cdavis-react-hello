c772335dd0646281bc61a3ee55ebcdcb5d9f84c6
cdavis2223 <cdavis2223@gmail.com>
2023-12-18 04:57:43 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,38 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-//include images into your bundle
+// include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+// create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center">
+      <h1 className="text-center mt-5">Hello Rigo!</h1>
+      <p>
+        <img src={rigoImage} alt="Rigo Baby" />
+      </p>
+      <a href="#" className="btn btn-success">
+        If you see this green button... bootstrap is working...
+      </a>
+      <p>
+        Made by{" "}
+        <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with love!
+      </p>
+    </div>
+  );
 };
 
-export default Home;
+const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+	</nav>
+  );
+};
+
+Navbar.propTypes = {
+  navbarUrl: PropTypes.string,
+  navbarLabel: PropTypes.string,
+};
+
+export { Home, Navbar }; // Use named exports instead of export default
